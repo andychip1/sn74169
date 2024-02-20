059fbda8b3cd4005d98137dbd922e206e8dce12b
andychip1 <160371219+andychip1@users.noreply.github.com>
2024-02-19 23:45:54 -0500

    Update sn74169.v

--- src/sn74169.v
+++ src/sn74169.v
@@ -1,11 +1,9 @@
 module sn74169(
            input  [3:0] A,  
            input  U_DB, input CLK, input ENPB, input ENTB, input LOADB,
-	   output [3:0] Q,
+	   output reg [3:0] Q,
 	   output RCOB
     );
-
-reg [3:0] Q;
 
  // loadb=0: load
  // NOR(enpb, entb) and U_DB: count up
