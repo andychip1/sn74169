feb42dda58705fc5cb6f59ee16a22ca226d408eb
andychip1 <160371219+andychip1@users.noreply.github.com>
2024-02-20 20:54:49 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Andrew Chen 
  * SPDX-License-Identifier: Apache-2.0
  */
 
@@ -19,11 +19,10 @@ module tt_um_andychip1_sn74169 (
   // All output pins must be assigned. If not used, assign to 0.
   assign uio_out = 0;
   assign uio_oe  = 0;
-  assign uo_out[7:5] = 3'b000;
+  assign uo_out[7:6] = 2'b00;
+  assign uo_out[5] = !ui_in[0];
 
 sn74169 i1(.A(ui_in[3:0]),.CLK(clk),.ENPB(ui_in[4]),.ENTB(ui_in[5]),.LOADB(ui_in[6]),.Q(uo_out[3:0]),.RCOB(uo_out[4]),.U_DB(ui_in[7]));
 
 
 endmodule
-
-
