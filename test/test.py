9d6405c12c736f7e167a49e6ed23f337a64a005f
andychip1 <160371219+andychip1@users.noreply.github.com>
2024-04-18 19:04:01 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -21,12 +21,14 @@ async def test_ac(dut):
   dut.rst_n.value = 0
   await ClockCycles(dut.clk, 2)
   dut.rst_n.value = 1
+  dut._log.info(dut.uo_out.value)
   dut._log.info("Load")
   await ClockCycles(dut.clk, 2)
+  dut._log.info(dut.uo_out.value)
   dut.ui_in.value = 192
   dut._log.info("Up6")
   await ClockCycles(dut.clk, 6)
-  
-  assert dut.uo_out.value == 16
   dut._log.info(dut.uo_out.value)
+  assert dut.uo_out.value == 16
+  
   
