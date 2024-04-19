cdab2c267cc94306c23f4e0c6a62559c30e6bed3
andychip1 <160371219+andychip1@users.noreply.github.com>
2024-04-18 20:33:53 -0400

    Update test.py

--- test/test.py
+++ test/test.py
@@ -14,7 +14,7 @@ async def test_ac(dut):
   cocotb.start_soon(clock.start())
 
   # Load
-  dut._log.info("ResetLoad")
+  dut._log.info("ResetLoad in=131= 1000 0011")
   dut.ena.value = 1
   dut.ui_in.value = 131
   dut.uio_in.value = 0
@@ -23,13 +23,24 @@ async def test_ac(dut):
   dut.rst_n.value = 1
   dut._log.info(dut.uo_out.value)
   dut._log.info("Load")
-  await ClockCycles(dut.clk, 2)
+  await ClockCycles(dut.clk, 1)
+  dut._log.info(dut.uo_out.value)
+  await ClockCycles(dut.clk, 1)
   dut._log.info(dut.uo_out.value)
   dut.ui_in.value = 192
   dut._log.info("Up6")
-  await ClockCycles(dut.clk, 6)
+  await ClockCycles(dut.clk, 1)
   dut._log.info(dut.uo_out.value)
-  
+  await ClockCycles(dut.clk, 1)
+  dut._log.info(dut.uo_out.value)
+  await ClockCycles(dut.clk, 1)
+  dut._log.info(dut.uo_out.value)
+  await ClockCycles(dut.clk, 1)
+  dut._log.info(dut.uo_out.value)
+  await ClockCycles(dut.clk, 1)
+  dut._log.info(dut.uo_out.value)
+  await ClockCycles(dut.clk, 1)
+  dut._log.info(dut.uo_out.value)
   # 56 = 0011 1000  -- value 8 + RCOB*16 + !ui_in[0]*32
   assert dut.uo_out.value == 56
   
